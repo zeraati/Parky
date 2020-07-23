b5750517d8c4d2a64afbd750a364a1f98f542dc6
Zeraati <Zeraatimail@gmail.com>
2020-07-24 00:54:32 +0430

    05

--- ParkyAPI/Models/Dto/NationalParkDto.cs
+++ ParkyAPI/Models/Dto/NationalParkDto.cs
@@ -5,11 +5,8 @@ namespace ParkyAPI.Models.Dto
 {
     public class NationalParkDto
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string State { get; set; }
         public DateTime Created { get; set; }
         public byte[] Picture { get; set; }
---
+++ ParkyAPI/Models/Mapper/NationalParkMapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyAPI.Models.Dto;
+
+namespace ParkyAPI.Models.Mapper
+{
+    public class NationalParkMapper
+    {
+        public static NationalPark Map(NationalParkDto dto)
+        {
+            return new NationalPark
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                State = dto.State,
+                Created = dto.Created,
+                Picture = dto.Picture,
+                Established = dto.Established
+            };
+        }
+
+        public static NationalParkDto Map(NationalPark model)
+        {
+            return new NationalParkDto
+            {
+                Id = model.Id,
+                Name = model.Name,
+                State = model.State,
+                Created = model.Created,
+                Picture = model.Picture,
+                Established = model.Established
+            };
+        }
+
+        public static void Map(NationalParkDto dto, NationalPark model)
+        {
+            model.Id = dto.Id;
+            model.Name = dto.Name;
+            model.State = dto.State;
+            model.Created = dto.Created;
+            model.Picture = dto.Picture;
+            model.Established = dto.Established;
+        }
+    }
+}
