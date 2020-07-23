a2d438cdb74b9591258d894ecc2115d396335aec
Zeraati <Zeraatimail@gmail.com>
2020-07-24 00:02:51 +0430

    01

---
+++ ParkyAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyAPI.Models;
+
+// ReSharper disable once CheckNamespace
+namespace ParkyAPI.Repository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool NationalParkExists(string nationalParkName);
+        bool NationalParkExists(int nationalParkId);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
+    }
+}
---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyAPI.Models;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository: INationalParkRepository
+    {
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NationalParkExists(string nationalParkName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NationalParkExists(int nationalParkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
