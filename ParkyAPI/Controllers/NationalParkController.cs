afd53756b3838ac3313309ced52ae5656c1a1301
Zeraati <Zeraatimail@gmail.com>
2020-07-24 02:33:15 +0430

    01

---
+++ ParkyAPI/Controllers/NationalParkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Repositories.IRepositories;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParkController : ControllerBase
+    {
+        private readonly INationalParkRepository _nationalParkRepository;
+
+        public NationalParkController(INationalParkRepository nationalParkRepository)
+        {
+            _nationalParkRepository = nationalParkRepository;
+        }
+    }
+}
